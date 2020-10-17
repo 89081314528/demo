8ef64bb1d35f1e32e00a5ede3e9fb08e31d44e6c
julia <89081314528@mail.ru>
2020-10-17 14:18:57 +0300

    17/10 1

--- src/main/java/com/example/demo/controllers/DemoController2.java
+++ src/main/java/com/example/demo/controllers/DemoController2.java
@@ -1,6 +1,6 @@
 package com.example.demo.controllers;
 
-import com.example.demo.controllers.Employee;
+import com.example.demo.repozitaries.EmployeeRepositary;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,23 +11,31 @@ import java.util.List;
 //№ 2 find by name принимает параметром строку с именем и возвращает лист отфильтрованный по имени
 //№ 3 find all возвращает полный лист, без входных параметров
 // №1 - принт рекорд, вызывает find by name, find by name вызывает all
+//
+// создать класс employeeRepozitory в пакете repozitary
+// сделать в этом классе метод findall такой же как в контроллере
+//добавить в контроллер зависимость от employeeRepozitory
+// использовать в контроллере метод findall из employeeRepozitory
 @RestController
 
 public class DemoController2 {
     public DemoController2() throws FileNotFoundException {
+        this.employee = new EmployeeRepositary();
     }
-    public List<Employee> findAll() throws IOException {
-        BufferedReader writer = new BufferedReader(new FileReader("salary.csv"));
-        String line = writer.readLine();
-        List<Employee> employees = new ArrayList<>();
-        for (; line != null; line = writer.readLine()) {
-            employees.add(new Employee(line));
-        }
-        return employees;
-    }
+    private final EmployeeRepositary employee;
 
-    public List<Employee> findName(String name) throws IOException {
-        List<Employee> employeeAll = findAll();
+//    public List<Employee> findAll() throws IOException {
+//        BufferedReader writer = new BufferedReader(new FileReader("salary.csv"));
+//        String line = writer.readLine();
+//        List<Employee> employees = new ArrayList<>();
+//        for (; line != null; line = writer.readLine()) {
+//            employees.add(new Employee(line));
+//        }
+//        return employees;
+//    }
+
+    public List<Employee> findByName(String name) throws IOException {
+        List<Employee> employeeAll = employee.findAll();
         List<Employee> employeeByName = new ArrayList<>();
         for (int i = 0; i < employeeAll.size(); i++) {
             Employee employee = employeeAll.get(i);
@@ -40,7 +48,7 @@ public class DemoController2 {
 
     @RequestMapping("/salary/{name}")
     public String printRecord(@PathVariable String name) throws IOException {
-        List<Employee> employeeByName = findName(name);
+        List<Employee> employeeByName = findByName(name);
         String result = "";
         for (int i = 0; i < employeeByName.size(); i++) {
             Employee employee = employeeByName.get(i);
