b745b9e001a9af329927fe5fe7b04f7b2439d796
julia <89081314528@mail.ru>
2020-10-17 12:46:00 +0300

    17/10

---
+++ src/main/java/com/example/demo/controllers/DemoController2.java
@@ -0,0 +1,54 @@
+package com.example.demo.controllers;
+
+import com.example.demo.controllers.Employee;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+//№ 2 find by name принимает параметром строку с именем и возвращает лист отфильтрованный по имени
+//№ 3 find all возвращает полный лист, без входных параметров
+// №1 - принт рекорд, вызывает find by name, find by name вызывает all
+@RestController
+
+public class DemoController2 {
+    public DemoController2() throws FileNotFoundException {
+    }
+    public List<Employee> findAll() throws IOException {
+        BufferedReader writer = new BufferedReader(new FileReader("salary.csv"));
+        String line = writer.readLine();
+        List<Employee> employees = new ArrayList<>();
+        for (; line != null; line = writer.readLine()) {
+            employees.add(new Employee(line));
+        }
+        return employees;
+    }
+
+    public List<Employee> findName(String name) throws IOException {
+        List<Employee> employeeAll = findAll();
+        List<Employee> employeeByName = new ArrayList<>();
+        for (int i = 0; i < employeeAll.size(); i++) {
+            Employee employee = employeeAll.get(i);
+            if (employee.getName().equals(name)) {
+                employeeByName.add(employee);
+            }
+        }
+        return employeeByName;
+    }
+
+    @RequestMapping("/salary/{name}")
+    public String printRecord(@PathVariable String name) throws IOException {
+        List<Employee> employeeByName = findName(name);
+        String result = "";
+        for (int i = 0; i < employeeByName.size(); i++) {
+            Employee employee = employeeByName.get(i);
+            result = result + employee.getMonth() + ";" + employee.getSalary() + "<br/>";
+        }
+        if (result.equals("")) {
+            result = "данных по имени " + name + " не найдено";
+        }
+        return result;
+    }
+}
